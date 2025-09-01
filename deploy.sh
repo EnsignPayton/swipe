4d5f914adfbcb819108e6629793566e7e05cd4e9
Logan Payton <Logan.Payton@javs.com>
2025-09-01 12:07:57 -0500

    wip

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 dotnet publish -c Release --self-contained
 echo "Creating output archive..."
-tar -czf bin/wowup.tar.gz -C bin/Release/net9.0/linux-x64/publish .
+tar -czf bin/wowup.tar.gz \
+	-C bin/Release/net9.0/linux-x64/publish . \
+	--owner=0 --group=0
 echo "Archive created at bin/wowup.tar.gz"

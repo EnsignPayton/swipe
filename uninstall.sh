fd025dc8acbe628f8ca61b9aea9dff08e744c931
Logan Payton <Logan.Payton@javs.com>
2026-02-08 16:16:48 -0500

    uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -2,7 +2,7 @@
 
 # TODO: Try to uninstall playwright too. Either delete the files directly or call playwright uninstall
 
-rm ~/.local/bin/wowup
+rm ~/.local/bin/swipe
 # TODO: uninstall.sh is located here. Do some magic to allow deleting itself. Copy to tmp and execute from there?
 # rm -rf ~/.local/share/wowup
 
